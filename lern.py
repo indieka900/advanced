462339cf078f841b13a96bfebd745d18783b6b0b
indieka900 <indiekaj@gmail.com>
2022-10-06 23:17:45 +0300

    Teaching python

---
+++ lern.py
@@ -0,0 +1,36 @@
+import time
+# age = int(input("Enter your age: "))
+# # print("You have entered:", name.upper())
+
+
+# url = "http//www.google.com"
+# slice = slice[9]
+# print(url[slice])
+
+# if (age>=18):
+#     print("Above 18")
+# elif (age<0):
+#     print("Not yet born")
+# else:
+#     print("Under age")
+
+#While loop
+# while True:
+#      name = input("Enter your name: ")
+#      if (name != ''):
+#         break
+i=0
+# while(i<20):
+#     print(i)
+#     i+=2
+
+#for loop
+# for k in range(0,20+1,2):
+#     print(k)
+#     time.sleep(1)
+
+# print("Happy new year")
+
+mine = "Joseph"
+for m in mine:
+    print(m)
